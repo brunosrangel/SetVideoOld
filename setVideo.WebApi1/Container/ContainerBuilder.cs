7c206c50004aafc90811837aa923b3eeb1dc843f
Bruno Rangel <bruno.rangel@wipro.com>
2020-06-20 14:47:22 -0300

    Teste

---
+++ setVideo.WebApi1/Container/ContainerBuilder.cs
@@ -0,0 +1,17 @@
+﻿using setVideo.Repository;
+using setVideo.Service;
+using Microsoft.Extensions.DependencyInjection;
+using setVideo.WebApi.Controllers;
+
+namespace setVideo.WebApi.Container
+{
+    public static class ContainerBuilder
+    {
+        public static void ConfigureContainer(IServiceCollection services)
+        {
+            services.AddTransient<ICustomerRepository, CustomerRepository>();
+            services.AddTransient<ICustomerServices, CustomerServices>();
+            services.AddTransient<ICustomerController, CustomerController>();
+        }
+    }
+}
---
+++ setVideo.WebApi1/Controllers/CustomerController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using setVideo.Model;
+using setVideo.Service;
+
+namespace setVideo.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class CustomerController : Controller , ICustomerController
+    {
+        private readonly ICustomerServices _customerServices;
+        public CustomerController(ICustomerServices customersService)
+        {
+            _customerServices = customersService;
+        }
+        [HttpGet("{id}")]
+        public Customer Get(int id) => _customerServices.GetById(id);
+
+        [HttpPost()]
+        public void Post(Customer customer) => _customerServices.Add(customer);
+
+
+    }
+
+    public interface ICustomerController
+    {
+        Customer Get(int id);
+        void Post(Customer customer);
+
+    }
+}

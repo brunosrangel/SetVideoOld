795d85063caa8ebeb15b1f3d45b1b7b9474b79f8
Bruno Rangel <bruno.rangel@wipro.com>
2020-06-22 04:05:56 -0300

    controle

---
+++ setVideo.Repository/setVideoContext.cs
@@ -0,0 +1,26 @@
+﻿using setVideo.Model;
+using setVideo.Repository.Configuration;
+using Microsoft.EntityFrameworkCore;
+
+namespace setVideo.Repository
+{
+
+    public class setVideoContext : DbContext
+    {
+  
+        public DbSet<Customer> Customers { get; set; }
+
+        public setVideoContext(DbContextOptions<setVideoContext> options) :
+            base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfiguration(new CustomerConfiguration());
+            modelBuilder.ApplyConfiguration(new MovieConfiguration());
+            modelBuilder.ApplyConfiguration(new LocationConfiguration());
+        }
+    }
+
+}
